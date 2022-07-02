7fb8600f6859e915d31f86711853b86aaf2c01fc
hongyang <hongyangprivate@gmail.com>
2022-07-02 20:06:57 +0800

    Initial commit

---
+++ src/main/java/io/hongyang/watabaseh/WatabasehApplication.java
@@ -0,0 +1,12 @@
+package io.hongyang.watabaseh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatabasehApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WatabasehApplication.class, args);
+  }
+}
